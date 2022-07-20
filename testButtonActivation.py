98ee75756a74c5f42eab3f3a0f44d4657360fe25
Boogsy679 <jothomas0615@gmail.com>
2022-07-20 13:48:59 -0400

    testing button activation using button states

---
+++ testButtonActivation.py
@@ -0,0 +1,29 @@
+import tkinter as tk
+from functools import partial
+
+arr = []
+
+def increaseArr(arrFlag):
+    arr.append(0)
+    print(arr)
+    if len(arr)>=5:
+        arrFlag.set('1')
+
+def activateButton(button, parFlag, c, d, e):
+    if parFlag.get() == '1':
+        button.config(state=tk.NORMAL)
+
+
+if __name__ == "__main__":
+    root = tk.Tk()
+    arrFlag = tk.StringVar()
+    arrFlag.set('0')
+    increaseMainFunc = partial(increaseArr, arrFlag)
+    increaseArrButton = tk.Button(root, text = "Increase Arr", command=increaseMainFunc)
+    increaseArrButton.pack()
+    saveButton = tk.Button(root, text = "Save and Exit", command = root.destroy, state=tk.DISABLED)
+    saveButton.pack()
+    activateButtonFunc = partial(activateButton, saveButton, arrFlag)
+    arrFlag.trace('w', activateButtonFunc)
+    root.mainloop()
+    print("OUT OF LOOP. Final Arr: "+ arr)
