f756b61447492bba9b50e1f958b276ae5fec7295
Boogsy679 <jothomas0615@gmail.com>
2022-07-26 14:06:14 -0400

    testing to have active yaml file on window

---
+++ gridTest.py
@@ -0,0 +1,28 @@
+import tkinter as tk
+from tkinter import filedialog
+import tkinter.ttk as ttk
+import math
+import platform
+import os.path
+from os import chdir, path
+import os
+from pathlib import Path
+from functools import partial
+from turtle import update
+from PIL import Image, ImageTk  # needed for images in gui
+
+def toplevel():
+    testWind = tk.Toplevel()
+    testWind.geometry("500x600")
+    left_label = tk.Label(testWind, text="This is a test left")
+    left_label.grid(row=0,column=0, sticky=tk.W)
+    right_label = tk.Label(testWind, text="This is a test right")
+    right_label.grid(row=0, column=1, sticky=tk.W)
+    testWind.mainloop()
+
+if __name__=="__main__":
+    root=tk.Tk()
+    root.geometry('500x600')
+    openTopLevel = tk.Button(root, text="Open toplevel", command=toplevel)
+    openTopLevel.pack()
+    root.mainloop()
