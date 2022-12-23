925ed252dd7d0a34d587659301478ece9a29c208
Boogsy679 <jothomas0615@gmail.com>
2022-12-23 10:20:21 -0500

    moved get_file_name_next

--- gui.py
+++ gui.py
@@ -5,7 +5,6 @@ from tkinter import filedialog
 import tkinter.ttk as ttk
 import platform
 from os import chdir, path
-import os
 from pathlib import Path
 from functools import partial
 from PIL import Image, ImageTk  # needed for images in gui
@@ -240,60 +239,7 @@ def tl():  # cycles through the options for the time limit option
         timeLimit.set("500")
 
 
-def get_file_name_next():  # checks to see if the file name the user selects exists or is empty
-    """Reads the file name and puts a message on the window if invalid characters are found
-    the file is empty, or if the file inputted already exists"""
-    inv_char_found = []
-    output_inv = ''
-    c=1
-    for i in fileName.get():
-        if i in invalidFileChar:
-            inv_char_found.append(i)
-    if len(inv_char_found)>0:
-        output_inv+="\""+inv_char_found[0]+"\""
-        for i in inv_char_found[1:]:
-            c+=1
-            if c==len(inv_char_found):
-                output_inv+=", and \""+i+"\""
-            else:
-                output_inv+=", \""+i+"\""
-    if len(inv_char_found)!=0 and invalidFlag.get()=='0':
-        for label in nameLabels:
-            label.destroy()
-        nameLabels.clear()
-        invalid_char_label = tk.Label(getFileName, text="The name entered contains invalid characters: "+output_inv)
-        invalid_char_label.pack()
-        nameLabels.append(invalid_char_label)
-        invalidFlag.set('1')
-        reqFlag.set('0')
-        existFlag.set('0')
-        inv_char_found.clear()
-        output_inv = ''
-        c=0
-    elif fileName.get() == "" and reqFlag.get() == "0":
-        for label in nameLabels:
-            label.destroy()
-        nameLabels.clear()
-        req_label = tk.Label(getFileName, text="This field is required. Please create a file")
-        req_label.pack()
-        nameLabels.append(req_label)
-        reqFlag.set('1')
-        invalidFlag.set('0')
-        existFlag.set('0')
-    elif (path.exists(fileName.get()) or path.exists(fileName.get() + '.yaml')) and existFlag.get() == '0':
-        for label in nameLabels:
-            label.destroy()
-        nameLabels.clear()
-        exist_label = tk.Label(getFileName,
-                               text="A file with this name already exists. Please enter another file name.")
-        exist_label.pack()
-        nameLabels.append(exist_label)
-        existFlag.set('1')
-        invalidFlag.set('0')
-        reqFlag.set('0')
-    elif fileName.get() != '' and not (path.exists(fileName.get()) or path.exists(fileName.get() + '.yaml')) and invalidFlag.get()!='1':
-        getFileName.destroy()
-    invalidFlag.set('0')
+
 
 
 def add_product():  # adds a product in agv_infos
@@ -964,7 +910,8 @@ if __name__ == "__main__":
     cancel_file = partial(cancel_wind, getFileName, cancelFlag)
     cancelFile = tk.Button(getFileName, text="Cancel and Exit", command=cancel_file)
     cancelFile.pack(side=tk.BOTTOM, pady=20)
-    fileExit = tk.Button(getFileName, text="Next", command=get_file_name_next)
+    fileFunc=partial(get_file_name_next, fileName, invalidFlag, nameLabels, getFileName, reqFlag, existFlag)
+    fileExit = tk.Button(getFileName, text="Next", command=fileFunc)
     fileExit.pack(side=tk.BOTTOM, pady=20)
     fileName.trace('w', fileNameCorrectFunc)
     getFileName.mainloop()
