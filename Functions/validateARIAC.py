66dcae9367f2aac041a7b36d1b444b4f857a1f25
Boogsy679 <jothomas0615@gmail.com>
2022-12-27 09:12:08 -0500

    removed print statements

--- Functions/validateARIAC.py
+++ Functions/validateARIAC.py
@@ -4,6 +4,6 @@ import json
 import yaml
 def validateAriac(yamlFile, schemaFile):
     schema=yaml.full_load(schemaFile)  # reads in the schema file as a json file
-    for i in schema['properties']:
-        print(i)
+    '''for i in schema['properties']:
+        print(i)'''
     validate(yamlFile, schema)
