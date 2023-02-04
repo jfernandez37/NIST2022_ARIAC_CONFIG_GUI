a59456841f1dc01177534915aa379c991a2b7e9f
Boogsy679 <jothomas0615@gmail.com>
2023-02-04 16:45:50 -0500

    added challenge tab to notebook

--- testing/notebookTesting.py
+++ testing/notebookTesting.py
@@ -49,50 +49,21 @@ def savePartOption(partEntry, partWidgets, partFlag, partVals, chosenOptions):
     switchPartMenu(partEntry, partVals, partWidgets, partFlag)
     print(chosenOptions)
 
-def runMainWind(chosenOptions,timeVal):
-    mainWind=tk.Tk()
-    frameFlag=tk.StringVar()
-    frameFlag.set('0')
-    mainWind.geometry('400x500')
-    mainWind.title('notebook testing')
-
-    notebook=ttk.Notebook(mainWind)
-    notebook.pack(pady=10, expand=True)
-
-    frame1 = ttk.Frame(notebook, width=400, height=280)
-    
-
-    frame1.pack(fill='both', expand=True)
-    
-
-    timeLabel=tk.Label(frame1, text="This is a time label test")
-    timeLabel.pack()
-
-    timeVar=tk.StringVar()
-    timeTest(frame1, timeVar, timeVal[0])
-    # add frames to notebook
-
-    notebook.add(frame1, text='Time')
-    if frameFlag.get()=="0":
-        frame2 = ttk.Frame(notebook, width=400, height=280)
-        frame2.pack(fill='both', expand=True)
-        notebook.add(frame2, text='Parts')
-    partFlag=tk.StringVar()
-    partFlag.set('0')
+def partsWidgets(partsFrame, partFlag):
     partOptionFlag=tk.StringVar()
     partOptionFlag.set('0')
     partVal=tk.StringVar()
     partVal.set('')
-    partEntry=tk.Entry(frame2, textvariable=partVal)
+    partEntry=tk.Entry(partsFrame, textvariable=partVal)
     partEntry.pack()
     partVals=[]
     partWidgets=[]
     #agv selection
     agvSelection=tk.StringVar()
     agvSelection.set(agvList[0])
-    agvSelectLabel=tk.Label(frame2, text="Select the agv for the part")
+    agvSelectLabel=tk.Label(partsFrame, text="Select the agv for the part")
     agvSelectLabel.pack_forget()
-    agvSelectMenu=tk.OptionMenu(frame2, agvSelection, *agvList)
+    agvSelectMenu=tk.OptionMenu(partsFrame, agvSelection, *agvList)
     agvSelectMenu.pack_forget()
     partVals.append(agvSelection)
     partWidgets.append(agvSelectLabel)
@@ -100,9 +71,9 @@ def runMainWind(chosenOptions,timeVal):
     #part type selection
     partType=tk.StringVar()
     partType.set(partTypes[0])
-    partTypeSelectLabel=tk.Label(frame2, text="Select the type of part")
+    partTypeSelectLabel=tk.Label(partsFrame, text="Select the type of part")
     partTypeSelectLabel.pack_forget()
-    partTypeSelectMenu=tk.OptionMenu(frame2, partType, *partTypes)
+    partTypeSelectMenu=tk.OptionMenu(partsFrame, partType, *partTypes)
     partTypeSelectMenu.pack_forget()
     partVals.append(partType)
     partWidgets.append(partTypeSelectLabel)
@@ -110,9 +81,9 @@ def runMainWind(chosenOptions,timeVal):
     #part color selection
     partColor=tk.StringVar()
     partColor.set(partColors[0])
-    partColorSelectLabel=tk.Label(frame2, text="Select the color of the part")
+    partColorSelectLabel=tk.Label(partsFrame, text="Select the color of the part")
     partColorSelectLabel.pack_forget()
-    partColorSelectMenu=tk.OptionMenu(frame2, partColor, *partColors)
+    partColorSelectMenu=tk.OptionMenu(partsFrame, partColor, *partColors)
     partColorSelectMenu.pack_forget()
     partVals.append(partColor)
     partWidgets.append(partColorSelectLabel)
@@ -120,21 +91,73 @@ def runMainWind(chosenOptions,timeVal):
     #rotation entry
     partRotation=tk.StringVar()
     partRotation.set('0')
-    partRotationLabel=tk.Label(frame2, text="Enter the rotation of the part")
+    partRotationLabel=tk.Label(partsFrame, text="Enter the rotation of the part")
     partRotationLabel.pack_forget()
-    partRotationEntry=tk.Entry(frame2, textvariable=partRotation)
+    partRotationEntry=tk.Entry(partsFrame, textvariable=partRotation)
     partRotationEntry.pack_forget()
     partVals.append(partRotation)
     partWidgets.append(partRotationLabel)
     partWidgets.append(partRotationEntry)
     show_option_menu=partial(switchPartMenu, partEntry, partVals, partWidgets, partFlag)
-    switchPartMenuButton=tk.Button(frame2, text="Switch Window", command=show_option_menu)
+    switchPartMenuButton=tk.Button(partsFrame, text="Switch Window", command=show_option_menu)
     switchPartMenuButton.pack()
     save_option=partial(savePartOption, partEntry, partWidgets, partFlag, partVals, chosenOptions)
-    saveOptionButton=tk.Button(frame2, text="Save option", command=save_option)
+    saveOptionButton=tk.Button(partsFrame, text="Save option", command=save_option)
     saveOptionButton.pack_forget()
     switch_buttons=partial(showAndHideButton,switchPartMenuButton, saveOptionButton, partVals[0], partOptionFlag)
     partVals[0].trace('w',switch_buttons)
+
+def printBlank():
+    print("")
+
+def chooseChallenge(challengeFrame):
+    newRobotMalfunctionButton=tk.Button(challengeFrame, text="Add new robot malfunction")
+    newRobotMalfunctionButton.grid(column=1)
+    newFaultyPartButton=tk.Button(challengeFrame, text="Add new faulty part")
+    newFaultyPartButton.grid(column=1)
+    newDroppedPartButton=tk.Button(challengeFrame, text="Add new dropped part")
+    newDroppedPartButton.grid(column=1)
+    newSensorBlackoutButton=tk.Button(challengeFrame, text="Add new sensor blackout")
+    newSensorBlackoutButton.grid(column=1)
+
+def runMainWind(chosenOptions,timeVal):
+    mainWind=tk.Tk()
+    mainWind.geometry('400x500')
+    mainWind.title('notebook testing')
+
+    notebook=ttk.Notebook(mainWind)
+    notebook.pack(pady=10, expand=True)
+
+    timeFrame = ttk.Frame(notebook, width=400, height=280)
+    
+
+    timeFrame.pack(fill='both', expand=True)
+    
+
+    timeLabel=tk.Label(timeFrame, text="This is a time label test")
+    timeLabel.pack()
+
+    timeVar=tk.StringVar()
+    timeTest(timeFrame, timeVar, timeVal[0])
+    # add frames to notebook
+
+    notebook.add(timeFrame, text='Time')
+    partsFrame = ttk.Frame(notebook, width=400, height=280)
+    partsFrame.pack(fill='both', expand=True)
+    notebook.add(partsFrame, text='Parts')
+
+    challengesFrame=ttk.Frame(notebook, width=400, height=280)
+    challengesFrame.pack(fill='both', expand=True)
+    notebook.add(challengesFrame, text="Challenges")
+
+    #Parts frame
+    partFlag=tk.StringVar()
+    partFlag.set('0')
+    partsWidgets(partsFrame, partFlag)
+
+    #Challenges frame
+    chooseChallenge(challengesFrame)
+
     mainWind.mainloop()
 
 if __name__=="__main__":
