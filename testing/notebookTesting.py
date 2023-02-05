dc797da3f53d727d7ac8f587c80569cc28f6e36e
Boogsy679 <jothomas0615@gmail.com>
2023-02-05 17:04:57 -0500

    added menus for challenges

--- testing/notebookTesting.py
+++ testing/notebookTesting.py
@@ -7,6 +7,15 @@ partTypes=["sensor", "pump", "regulator", "battery"]
 partColors=['green', 'red', 'purple','blue','orange']
 trays=["Tray 0","Tray 1","Tray 2","Tray 3","Tray 4","Tray 5","Tray 6","Tray 7","Tray 8","Tray 9"]
 slots=["Slot 1", "Slot 2", "Slot 3", "Slot 4", "Slot 5", "Slot 6"]
+agvOptions=["1","2","3","4"]
+allPartTypes=["sensor", "pump", "regulator", "battery"]
+allPartColors=['green', 'red', 'purple','blue','orange']
+robotTypes=["ceiling_robot","floor_robot"]
+destinations=["warehouse", "as1", "as2","as3","as4","kitting"]
+stations=["as1","as2","as3","as4"]
+sensBOCategories=["time-based","during kitting", "during assembly","after kitting", "after assembly"]
+conditionTypes=['','time','partPlace','submission']
+usedIds=["ABCDFGHI"]
 LEFTCOLUMN=1
 MIDDLECOLUMN=2
 RIGHTCOLUMN=3
@@ -251,6 +260,20 @@ def kittingTrayWidgets(setupFrame, kittingTrayCounter, availableSlots, available
     slot4.trace('w', update_all_slot_menus)
     slot5.trace('w', update_all_slot_menus)
     slot6.trace('w', update_all_slot_menus)
+    trayVals.clear()
+    slotVals.clear()
+    trayVals.append(tray1.get())
+    trayVals.append(tray2.get())
+    trayVals.append(tray3.get())
+    trayVals.append(tray4.get())
+    trayVals.append(tray5.get())
+    trayVals.append(tray6.get())
+    slotVals.append(slot1.get())
+    slotVals.append(slot2.get())
+    slotVals.append(slot3.get())
+    slotVals.append(slot4.get())
+    slotVals.append(slot5.get())
+    slotVals.append(slot6.get())
 
 def timeEntry(timeFrame, timeVar, timeVal):
     timeVar.set(timeVal)
@@ -258,6 +281,7 @@ def timeEntry(timeFrame, timeVar, timeVal):
     timeLabel.grid(column=MIDDLECOLUMN)
     timeEntry=tk.Entry(timeFrame, textvariable=timeVar)
     timeEntry.grid(column=MIDDLECOLUMN)
+
 
 def removeFrame(frameFlag, mainWind):
     frameFlag.set('1')
@@ -356,9 +380,6 @@ def partsWidgets(partsFrame, partFlag):
     switch_buttons=partial(showAndHideButton,switchPartMenuButton, saveOptionButton, partVals[0], partOptionFlag)
     partVals[0].trace('w',switch_buttons)
 
-def printBlank():
-    print("")
-
 def chooseChallenge(challengeFrame):
     newRobotMalfunctionButton=tk.Button(challengeFrame, text="Add new robot malfunction")
     newRobotMalfunctionButton.grid(column=1)
@@ -369,7 +390,154 @@ def chooseChallenge(challengeFrame):
     newSensorBlackoutButton=tk.Button(challengeFrame, text="Add new sensor blackout")
     newSensorBlackoutButton.grid(column=1)
 
+def allChallengeWidgets(challengesFrame):
+    #robot malfunction
+    duration=tk.StringVar()
+    duration.set("0")
+    durationLabel=tk.Label(challengesFrame, text="Enter the duration of the robot malfunction")
+    durationLabel.grid_forget()
+    durationEntry=tk.Entry(challengesFrame, textvariable=duration)
+    durationEntry.grid_forget()
+    floorRobot=tk.StringVar()
+    floorRobot.set("0")
+    ceilRobot=tk.StringVar()
+    ceilRobot.set("0")
+    floorRobotCB=tk.Checkbutton(challengesFrame, text="Floor robot", variable=floorRobot, onvalue="1", offvalue="0", height=1, width=20)
+    floorRobotCB.grid_forget()
+    ceilRobotCB=tk.Checkbutton(challengesFrame, text="Ceiling robot", variable=ceilRobot, onvalue="1", offvalue="0", height=1, width=20)
+    ceilRobotCB.grid_forget()
+    #faulty Part
+    currentOrderID=tk.StringVar()
+    currentOrderID.set(usedIds[0])
+    orderIDLabel=tk.Label(challengesFrame, text="Select the order ID for the faulty part")
+    orderIDLabel.grid_forget()
+    orderIDMenu=tk.OptionMenu(challengesFrame, currentOrderID, *usedIds)
+    orderIDMenu.grid_forget()
+    q1=tk.StringVar()
+    q1.set('0')
+    q2=tk.StringVar()
+    q2.set('0')
+    q3=tk.StringVar()
+    q3.set('0')
+    q4=tk.StringVar()
+    q4.set('0')
+    q1CB=tk.Checkbutton(challengesFrame, text="Quadrant 1", variable=q1, onvalue="1", offvalue="0", height=1, width=20)
+    q1CB.grid_forget()
+    q2CB=tk.Checkbutton(challengesFrame, text="Quadrant 2", variable=q2, onvalue="1", offvalue="0", height=1, width=20)
+    q2CB.grid_forget()
+    q3CB=tk.Checkbutton(challengesFrame, text="Quadrant 3", variable=q3, onvalue="1", offvalue="0", height=1, width=20)
+    q3CB.grid_forget()
+    q4CB=tk.Checkbutton(challengesFrame, text="Quadrant 4", variable=q4, onvalue="1", offvalue="0", height=1, width=20)
+    q4CB.grid_forget()
+    #dropped part
+    robotType=tk.StringVar()
+    robotType.set(robotTypes[0])
+    robotTypeLabel=tk.Label(challengesFrame, text="Select the robot type for the dropped part")
+    robotTypeLabel.grid_forget()
+    robotTypeMenu=tk.OptionMenu(challengesFrame, robotType, *robotTypes)
+    robotTypeMenu.grid_forget()
+    partType=tk.StringVar()
+    partType.set(allPartTypes[0])
+    partTypeLabel=tk.Label(challengesFrame, text="Select the type of part")
+    partTypeLabel.grid_forget()
+    partTypeMenu=tk.OptionMenu(challengesFrame, partType, *allPartTypes)
+    partTypeMenu.grid_forget()
+    partColor=tk.StringVar()
+    partColor.set(allPartColors[0])
+    partColorLabel=tk.Label(challengesFrame, text="Select the color of the part")
+    partColorLabel.grid_forget()
+    partColorMenu=tk.OptionMenu(challengesFrame, partColor, *allPartColors)
+    partColorMenu.grid_forget()
+    dropAfterNum=tk.StringVar()
+    dropAfterNum.set("0")
+    dropAfterNumLabel=tk.Label(challengesFrame, text="Set the number to drop the part after")
+    dropAfterNumLabel.grid_forget()
+    dropAfterNumEntry=tk.Entry(challengesFrame, textvariable=dropAfterNum)
+    dropAfterNumEntry.grid_forget()
+    dropAfterTime=tk.StringVar()
+    dropAfterTime.set('0')
+    dropAfterTimeLabel=tk.Label(challengesFrame,text="Set the time to drop the part after")
+    dropAfterTimeLabel.grid_forget()
+    dropAfterTimeEntry=tk.Entry(challengesFrame, textvariable=dropAfterTime)
+    dropAfterTimeEntry.grid_forget()
+    #sensor blackout
+    category=tk.StringVar()
+    category.set(sensBOCategories[0])
+    categoryLabel=tk.Label(challengesFrame, text="Choose the category for the sensor blackout")
+    categoryLabel.grid_forget()
+    categoryMenu=tk.OptionMenu(challengesFrame, category, *sensBOCategories)
+    categoryMenu.grid_forget()
+    duration=tk.StringVar()
+    duration.set('0')
+    durationLabel=tk.Label(challengesFrame, text="Enter the duration for the sensor blackout")
+    durationLabel.grid_forget()
+    durationEntry=tk.Entry(challengesFrame, textvariable=duration)
+    durationEntry.grid_forget()
+    sensor1=tk.StringVar()
+    sensor2=tk.StringVar()
+    sensor3=tk.StringVar()
+    sensor4=tk.StringVar()
+    sensor5=tk.StringVar()
+    sensor6=tk.StringVar()
+    sensor1.set('0')
+    sensor2.set('0')
+    sensor3.set('0')
+    sensor4.set('0')
+    sensor5.set('0')
+    sensor6.set('0')
+    sensor1CB=tk.Checkbutton(challengesFrame, text="break beam", variable=sensor1, onvalue="1", offvalue="0", height=1, width=20)
+    sensor1CB.grid_forget()
+    sensor2CB=tk.Checkbutton(challengesFrame, text="proximity", variable=sensor2, onvalue="1", offvalue="0", height=1, width=20)
+    sensor2CB.grid_forget()
+    sensor3CB=tk.Checkbutton(challengesFrame, text="laser profiler", variable=sensor3, onvalue="1", offvalue="0", height=1, width=20)
+    sensor3CB.grid_forget()
+    sensor4CB=tk.Checkbutton(challengesFrame, text="lidar", variable=sensor4, onvalue="1", offvalue="0", height=1, width=20)
+    sensor4CB.grid_forget()
+    sensor5CB=tk.Checkbutton(challengesFrame, text="camera", variable=sensor5, onvalue="1", offvalue="0", height=1, width=20)
+    sensor5CB.grid_forget()
+    sensor6CB=tk.Checkbutton(challengesFrame, text="logical camera", variable=sensor6, onvalue="1", offvalue="0", height=1, width=20)
+    sensor6CB.grid_forget()
+    #condition
+    condition=tk.StringVar()
+    condition.set(conditionTypes[0])
+    conditionLabel=tk.Label(challengesFrame, text="Select a condition for the order")
+    conditionLabel.grid_forget()
+    conditionMenu=tk.OptionMenu(challengesFrame, condition, *conditionTypes)
+    conditionMenu.grid_forget()
+    conTime=tk.StringVar()
+    conTime.set('')
+    conTimeLabel=tk.Label(challengesFrame, text="Enter the time")
+    conTimeLabel.grid_forget()
+    conTimeEntry=tk.Entry(challengesFrame, textvariable=conTime)
+    conTimeEntry.grid_forget()
+    conAgv=tk.StringVar()
+    conAgv.set("")
+    conAgvLabel=tk.Label(challengesFrame, text="Choose the agv")
+    conAgvLabel.grid_forget()
+    conAgvMenu=tk.OptionMenu(challengesFrame, conAgv, *agvOptions)
+    conAgvMenu.grid_forget()
+    conPartType=tk.StringVar()
+    conPartType.set("")
+    conPartTypeLabel=tk.Label(challengesFrame, text="Select the type of part")
+    conPartTypeLabel.grid_forget()
+    conPartTypeMenu=tk.OptionMenu(challengesFrame, conPartType, *allPartTypes)
+    conPartTypeMenu.grid_forget()
+    conPartColor=tk.StringVar()
+    conPartColor.set("")
+    conPartColorLabel=tk.Label(challengesFrame, text="Select the color of the part")
+    conPartColorLabel.grid_forget()
+    conPartColorMenu=tk.OptionMenu(challengesFrame, conPartColor, *allPartColors)
+    conPartColorMenu.grid_forget()
+    annID=tk.StringVar()
+    annID.set("")
+    annIDLabel=tk.Label(challengesFrame, text="Select the order ID")
+    annIDLabel.grid_forget()
+    annIDMenu=tk.OptionMenu(challengesFrame, annID, *usedIds)
+    annIDMenu.grid_forget()
+
 def runMainWind(chosenOptions,timeVal):
+    presentChallengeWidgets=[]
+    allChallengeWidgets=[]
     trayVals=[]
     slotVals=[]
     availableTrays=["Tray 0","Tray 1","Tray 2","Tray 3","Tray 4","Tray 5","Tray 6","Tray 7","Tray 8","Tray 9"]
